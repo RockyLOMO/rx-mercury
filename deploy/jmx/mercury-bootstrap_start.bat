551ac9ae4a4ee2570dd092a44d36a6c2c5c75520
℞ <1748083+RockyLOMO@users.noreply.github.com>
2022-10-09 18:37:17 +0800

    up

--- deploy/jmx/mercury-bootstrap_start.bat
+++ deploy/jmx/mercury-bootstrap_start.bat
@@ -11,5 +11,5 @@ if exist "C:\app-crawler\mercury-bootstrap-1.0.jar.publish" (
 
 cd c:\app-crawler\
 set JAR_PATH=.\mercury-bootstrap-1.0.jar
-set MEM_OPTIONS=-Xms128m -Xmx256m -Xss512k -XX:MetaspaceSize=128m -XX:MaxMetaspaceSize=128m -XX:MaxDirectMemorySize=256m -Dapollo.configService=http://db.f-li.cn:8080 -Denv=pro
+set MEM_OPTIONS=-Xms128m -Xmx128m -Xss512k -XX:MetaspaceSize=128m -XX:MaxMetaspaceSize=128m -XX:MaxDirectMemorySize=256m -Dapollo.configService=http://db.f-li.cn:8080 -Denv=pro
 java -javaagent:.\jmx_prometheus_javaagent.jar=8080:.\jmx_prometheus_config.yaml %MEM_OPTIONS% -Dspring.profiles.active=prd -Dfile.encoding=UTF-8 -jar %JAR_PATH%
