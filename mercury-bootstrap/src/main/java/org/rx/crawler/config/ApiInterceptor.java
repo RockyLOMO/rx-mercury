e80fc73e7627fc8907ef44775bea304788d56bbd
℞ <1748083+RockyLOMO@users.noreply.github.com>
2022-10-13 22:09:48 +0800

    Create ApiInterceptor.java

---
+++ mercury-bootstrap/src/main/java/org/rx/crawler/config/ApiInterceptor.java
@@ -0,0 +1,17 @@
+package org.rx.crawler.config;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.rx.spring.ControllerInterceptor;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+public class ApiInterceptor extends ControllerInterceptor {
+    @Around("execution(public * org.rx.crawler.controller..*.*(..))")
+    @Override
+    public Object doAround(ProceedingJoinPoint joinPoint) throws Throwable {
+        return super.doAround(joinPoint);
+    }
+}
