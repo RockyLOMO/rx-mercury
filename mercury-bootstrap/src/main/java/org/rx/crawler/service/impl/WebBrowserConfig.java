996ec01522d500c1962c29ddd2dadf358ac3767e
youfan <115806199+youfanx@users.noreply.github.com>
2022-11-18 11:13:53 +0800

    up

--- mercury-bootstrap/src/main/java/org/rx/crawler/service/impl/WebBrowserConfig.java
+++ mercury-bootstrap/src/main/java/org/rx/crawler/service/impl/WebBrowserConfig.java
@@ -7,6 +7,7 @@ import org.rx.crawler.service.CookieContainer;
 
 @Data
 public class WebBrowserConfig {
+    private long waitMillis = 500;
     private int pageLoadTimeoutSeconds = 30;
     private int findElementTimeoutSeconds = 6;
     private String diskDataPath;
